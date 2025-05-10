9b40522064d0fb3d2670af6b1600f6d17f1cf8a5
Geert Fannes <geert.fannes@auro-3d.com>
2025-05-10 16:14:04 +0200

    Rename

--- src/slice.zig
+++ src/slice.zig
@@ -7,14 +7,14 @@ pub fn is_empty(slice: anytype) bool {
 pub fn first(slice: anytype) ?@TypeOf(slice[0]) {
     return if (slice.len > 0) slice[0] else null;
 }
-pub fn first_ptr(slice: anytype) ?@TypeOf(&slice[0]) {
+pub fn firstPtr(slice: anytype) ?@TypeOf(&slice[0]) {
     return if (slice.len > 0) &slice[0] else null;
 }
 
 pub fn last(slice: anytype) ?@TypeOf(slice[0]) {
     return if (slice.len > 0) slice[slice.len - 1] else null;
 }
-pub fn last_ptr(slice: anytype) ?@TypeOf(&slice[0]) {
+pub fn lastPtr(slice: anytype) ?@TypeOf(&slice[0]) {
     return if (slice.len > 0) &slice[slice.len - 1] else null;
 }
 
@@ -27,16 +27,16 @@ test {
         slice = &.{};
         try ut.expectEqual(true, is_empty(slice));
         try ut.expectEqual(null, first(slice));
-        try ut.expectEqual(null, first_ptr(slice));
+        try ut.expectEqual(null, firstPtr(slice));
         try ut.expectEqual(null, last(slice));
-        try ut.expectEqual(null, last_ptr(slice));
+        try ut.expectEqual(null, lastPtr(slice));
     }
     {
         slice = "abc";
         try ut.expectEqual(false, is_empty(slice));
         try ut.expectEqual(@as(?u8, 'a'), first(slice));
-        try ut.expectEqual(@as(?u8, 'a'), (first_ptr(slice) orelse unreachable).*);
+        try ut.expectEqual(@as(?u8, 'a'), (firstPtr(slice) orelse unreachable).*);
         try ut.expectEqual(@as(?u8, 'c'), last(slice));
-        try ut.expectEqual(@as(?u8, 'c'), (last_ptr(slice) orelse unreachable).*);
+        try ut.expectEqual(@as(?u8, 'c'), (lastPtr(slice) orelse unreachable).*);
     }
 }
