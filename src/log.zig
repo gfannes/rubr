eb007001e2812d9993f37c1dd9d3c0b26a268adc
Geert Fannes <geert.fannes@auro-3d.com>
2025-06-27 09:37:33 +0200

    Support for adding process id the log file

--- src/log.zig
+++ src/log.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+pub const Error = error{FilePathTooLong};
+
 // &improv: Support both buffered and non-buffered logging
 pub const Log = struct {
     const Self = @This();
@@ -20,13 +22,43 @@ pub const Log = struct {
         self.closeWriter() catch {};
     }
 
+    // Any '%' in 'filepath' will be replaced with the process id
     pub fn toFile(self: *Self, filepath: []const u8) !void {
         try self.closeWriter();
 
-        if (std.fs.path.isAbsolute(filepath))
-            self._file = try std.fs.createFileAbsolute(filepath, .{})
+        var pct_count: usize = 0;
+        for (filepath) |ch| {
+            if (ch == '%')
+                pct_count += 1;
+        }
+
+        var buf: [std.fs.max_path_bytes]u8 = undefined;
+        const filepath_clean = if (pct_count > 0) blk: {
+            var pid_buf: [32]u8 = undefined;
+            const pid_str = try std.fmt.bufPrint(&pid_buf, "{}", .{std.c.getpid()});
+            if (filepath.len + pct_count * pid_str.len >= buf.len)
+                return Error.FilePathTooLong;
+            var ix: usize = 0;
+            for (filepath) |ch| {
+                if (ch == '%') {
+                    for (pid_str) |c| {
+                        buf[ix] = c;
+                        ix += 1;
+                    }
+                } else {
+                    buf[ix] = ch;
+                    ix += 1;
+                }
+            }
+            break :blk buf[0..ix];
+        } else blk: {
+            break :blk filepath;
+        };
+
+        if (std.fs.path.isAbsolute(filepath_clean))
+            self._file = try std.fs.createFileAbsolute(filepath_clean, .{})
         else
-            self._file = try std.fs.cwd().createFile(filepath, .{});
+            self._file = try std.fs.cwd().createFile(filepath_clean, .{});
         self._do_close = true;
 
         self.initWriter();
