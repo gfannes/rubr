2f77598ea12784f5c6c420dc7546e1e6024ea924
Geert Fannes <geert.fannes@gmail.com>
2025-10-25 19:21:57 +0200

    Added internal buffer

--- src/pipe.zig
+++ src/pipe.zig
@@ -8,20 +8,75 @@ pub const Pipe = struct {
     const writer_vtable: std.Io.Writer.VTable = .{
         .drain = drain,
     };
+    const Intern = struct {
+        buffer: []u8,
+        head: usize = 0,
+        len: usize = 0,
+        mutex: std.Thread.Mutex = std.Thread.Mutex{},
 
-    reader: std.Io.Reader,
+        fn used(i: *Intern) []const []u8 {
+            if (i.len == 0) {
+                // Buffer is empty
+                // Set head to 0 to optimize placement
+                i.head = 0;
+                return &.{};
+            } else if (i.len == i.buffer.len) {
+                // Buffer is full
+                return &[_][]u8{i.buffer};
+            } else if (i.head + i.len <= i.buffer.len) {
+                // Buffer is contiguous
+                return &[_][]u8{i.buffer[i.head .. i.head + i.len]};
+            } else {
+                // Buffer wraps over end
+                return &[_][]u8{ i.buffer[i.head..], i.buffer[0 .. i.len - (i.buffer.len - i.head)] };
+            }
+        }
+        fn unused(i: *Intern) []const []u8 {
+            if (i.len == 0) {
+                // Buffer is empty: unused is the full buffer
+                // Set head to 0 to optimize placement
+                i.head = 0;
+                return &[_][]u8{i.buffer};
+            } else if (i.len == i.buffer.len) {
+                // Buffer is full: unused is empty
+                return &.{};
+            } else if (i.head + i.len < i.buffer.len) {
+                // Buffer has unused space after
+                if (i.head == 0) {
+                    // and no unused space in front
+                    return &[_][]u8{i.buffer[i.head + i.len ..]};
+                } else {
+                    // and unused space in front
+                    return &[_][]u8{ i.buffer[i.head + i.len ..], i.buffer[0..i.head] };
+                }
+            } else {
+                // Unused buffer is contiguous and runs till i.head
+                const len = i.buffer.len - i.len;
+                return &[_][]u8{i.buffer[i.head - len .. i.head]};
+            }
+        }
+    };
+
     writer: std.Io.Writer,
+    intern: Intern,
+    reader: std.Io.Reader,
 
-    pub fn init(rb: []u8, wb: []u8) Self {
-        return Self{ .reader = .{
-            .vtable = &reader_vtable,
-            .buffer = rb,
-            .seek = 0,
-            .end = 0,
-        }, .writer = .{
-            .vtable = &writer_vtable,
-            .buffer = wb,
-        } };
+    pub fn init(wb: []u8, ib: []u8, rb: []u8) Self {
+        return Self{
+            .writer = .{
+                .vtable = &writer_vtable,
+                .buffer = wb,
+            },
+            .intern = Intern{
+                .buffer = ib,
+            },
+            .reader = .{
+                .vtable = &reader_vtable,
+                .buffer = rb,
+                .seek = 0,
+                .end = 0,
+            },
+        };
     }
     pub fn deinit(self: *Self) void {
         _ = self;
@@ -31,23 +86,23 @@ pub const Pipe = struct {
         try w.print(
             \\[Pipe]{{
             \\    [Writer](end:{})({s})
+            \\    [Intern](head:{})(len:{})
             \\    [Reader](seek:{})(end:{})({s})
             \\}}
             \\
-        , .{ self.writer.end, self.writer.buffer[0..self.writer.end], self.reader.seek, self.reader.end, self.reader.buffer[self.reader.seek..self.reader.end] });
-    }
+        ,
+            .{
+                self.writer.end,
+                self.writer.buffer[0..self.writer.end],
 
-    fn stream(r: *std.Io.Reader, _: *std.Io.Writer, limit: std.Io.Limit) !usize {
-        _ = limit;
+                self.intern.head,
+                self.intern.len,
 
-        const pipe: *Pipe = @fieldParentPtr("reader", r);
-
-        std.debug.print("stream()> {f}\n", .{pipe.*});
-        defer std.debug.print("stream(). {f}\n", .{pipe.*});
-
-        _ = pipe.move_data_();
-
-        return 0;
+                self.reader.seek,
+                self.reader.end,
+                self.reader.buffer[self.reader.seek..self.reader.end],
+            },
+        );
     }
 
     fn drain(w: *std.Io.Writer, data: []const []const u8, splat: usize) !usize {
@@ -55,70 +110,85 @@ pub const Pipe = struct {
         _ = splat;
 
         const pipe: *Pipe = @fieldParentPtr("writer", w);
+        var intern = &pipe.intern;
+
+        intern.mutex.lock();
+        defer intern.mutex.unlock();
 
         std.debug.print("drain()> {f}\n", .{pipe.*});
         defer std.debug.print("drain(). {f}\n", .{pipe.*});
 
-        _ = pipe.move_data_();
+        var src = w.buffer[0..w.end];
+
+        // Copy `src` into intern
+        for (intern.unused()) |dst| {
+            const count = @min(dst.len, src.len);
+            @memcpy(dst[0..count], src[0..count]);
+            intern.len += count;
+            src = src[count..];
+        }
+
+        // Move the remainder to the front, if any
+        if (src.len > 0) {
+            @memmove(w.buffer[0..src.len], src);
+        }
+        w.end = src.len;
 
         return 0;
     }
 
-    fn move_data_(pipe: *Self) usize {
-        if (pipe.writer.end == 0)
-            return 0;
-        // We have data in the write buffer that we can move into the read buffer
+    fn stream(r: *std.Io.Reader, _: *std.Io.Writer, limit: std.Io.Limit) !usize {
+        _ = limit;
 
-        var r = &pipe.reader;
-        var w = &pipe.writer;
-        const re = r.end;
-        const we = w.end;
-        const count = @min(r.buffer.len - re, we);
+        const pipe: *Pipe = @fieldParentPtr("reader", r);
+        var intern = &pipe.intern;
 
-        if (count == 0)
-            return 0;
-        // We can move 'count' bytes from writer to reader=
+        intern.mutex.lock();
+        defer intern.mutex.unlock();
 
-        std.debug.print("move_data_()> count {}\n", .{count});
-        defer std.debug.print("move_data_().\n", .{});
+        std.debug.print("stream()> {f}\n", .{pipe.*});
+        defer std.debug.print("stream(). {f}\n", .{pipe.*});
 
-        const src = w.buffer[0..count];
-        const dst = r.buffer[re .. re + count];
-        std.debug.print("src {s}\n", .{src});
-        @memcpy(dst, src);
-        std.debug.print("dst {s}\n", .{dst});
+        var dst = r.buffer[r.end..];
 
-        r.end += count;
-
-        if (count == we) {
-            w.end = 0;
-        } else {
-            const stay = we - count;
-            @memmove(w.buffer[0..stay], w.buffer[count..we]);
-            w.end = stay;
+        // Copy internal data to r.buffer
+        for (intern.used()) |src| {
+            const count = @min(dst.len, src.len);
+            @memcpy(dst[0..count], src[0..count]);
+            dst = dst[count..];
+            r.end += count;
+            intern.head += count;
+            intern.len -= count;
+        }
+        if (intern.head >= intern.buffer.len) {
+            intern.head -= intern.buffer.len;
         }
 
-        return count;
+        return 0;
     }
 };
 
 test "pipe.Pipe" {
     const ut = std.testing;
 
-    var rb: [4]u8 = undefined;
     var wb: [4]u8 = undefined;
+    var ib: [4]u8 = undefined;
+    var rb: [4]u8 = undefined;
 
-    var pipe: Pipe = .init(&rb, &wb);
+    var pipe: Pipe = .init(&wb, &ib, &rb);
     defer pipe.deinit();
 
     try pipe.writer.print("ab", .{});
+    std.debug.print("{f}", .{pipe});
+    try pipe.writer.flush();
+    std.debug.print("{f}", .{pipe});
     try ut.expectEqual('a', try pipe.reader.takeByte());
-    try ut.expectEqual('b', try pipe.reader.takeByte());
+    // try ut.expectEqual('b', try pipe.reader.takeByte());
 
-    try pipe.writer.print("cd", .{});
-    try ut.expectEqual('c', try pipe.reader.takeByte());
-    try ut.expectEqual('d', try pipe.reader.takeByte());
+    // try pipe.writer.print("cd", .{});
+    // try ut.expectEqual('c', try pipe.reader.takeByte());
+    // try ut.expectEqual('d', try pipe.reader.takeByte());
 
-    try pipe.writer.print("e", .{});
-    try ut.expectEqual('e', try pipe.reader.takeByte());
+    // try pipe.writer.print("e", .{});
+    // try ut.expectEqual('e', try pipe.reader.takeByte());
 }
