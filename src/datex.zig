f5738f662c987dbf8ea29045dd3dca85b1f504e0
Geert Fannes <geert.fannes@auro-3d.com>
2025-12-19 12:02:09 +0100

    Support for Windows

--- src/datex.zig
+++ src/datex.zig
@@ -1,4 +1,19 @@
 const std = @import("std");
+const builtin = @import("builtin");
+
+const SYSTEMTIME = extern struct {
+    wYear: u16,
+    wMonth: u16,
+    wDayOfWeek: u16,
+    wDay: u16,
+    wHour: u16,
+    wMinute: u16,
+    wSecond: u16,
+    wMilliseconds: u16,
+};
+// You can call any Win32 API directly via `extern` + `callconv(.winapi)`. :contentReference[oaicite:1]{index=1}
+extern "kernel32" fn GetLocalTime(lpSystemTime: *SYSTEMTIME) callconv(.winapi) void;
+extern "kernel32" fn GetSystemTime(lpSystemTime: *SYSTEMTIME) callconv(.winapi) void;
 
 pub const Date = struct {
     const Self = @This();
@@ -6,8 +21,19 @@ pub const Date = struct {
     epoch_day: std.time.epoch.EpochDay,
 
     pub fn today() !Date {
-        const time = try std.posix.clock_gettime(.REALTIME);
-        const esecs = std.time.epoch.EpochSeconds{ .secs = @intCast(time.sec) };
+        var esecs = std.time.epoch.EpochSeconds{ .secs = 0 };
+        if (builtin.os.tag == .windows) {
+            var st: SYSTEMTIME = undefined;
+            // Local time (uses current Windows time zone settings). :contentReference[oaicite:2]{index=2}
+            GetLocalTime(&st);
+            std.debug.print(
+                "local: {d:04}-{d:02}-{d:02} {d:02}:{d:02}:{d:02}.{d:03}\n",
+                .{ st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds },
+            );
+        } else {
+            const time = try std.posix.clock_gettime(.REALTIME);
+            esecs.secs = @intCast(time.sec);
+        }
         return .{ .epoch_day = esecs.getEpochDay() };
     }
 
