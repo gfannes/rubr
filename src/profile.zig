fbbfad7ff8987b337f74358ac473daa737e26b09
Geert Fannes <geert.fannes@auro-3d.com>
2026-09-01 21:02:33 +0200

    Support for measurement names

--- src/profile.zig
+++ src/profile.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 pub const Error = error{
     NotRunning,
     StillRunning,
+    IxOutOfRange,
+    NameMismatch,
 };
 
 pub fn now(io: std.Io) std.Io.Timestamp {
@@ -14,65 +16,102 @@ const Timestamp = i128;
 const Measurement = struct {
     pub const Self = @This();
 
+    name: ?[]const u8 = null,
     sum: Timestamp = 0,
     max: Timestamp = 0,
+};
 
-    pub fn format(self: Self, w: *std.Io.Writer) !void {
-        try w.print("[Measurement]", .{});
-        try format_(self.sum, "sum", w);
-        try format_(self.max, "max", w);
-        try w.flush();
-    }
-    fn format_(ts: Timestamp, desc: []const u8, w: *std.Io.Writer) !void {
-        const a = @divFloor(ts, 1_000_000_000);
-        const b = ts - a * 1_000_000_000;
-        try w.print("({s}:{}.{:0>9.9}s)", .{ desc, a, @as(u64, @intCast(b)) });
-    }
+const Descriptor = struct {
+    ix: usize,
+    name: ?[]const u8 = null,
 };
 
 pub const Scope = struct {
     const Self = @This();
 
-    pub const Id = enum { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z };
-
-    const count = @typeInfo(Id).@"enum".fields.len;
+    const max_measurement_count: usize = 16;
 
     io: std.Io,
 
-    id: Id,
+    ix: usize,
     start_ts: std.Io.Timestamp,
 
-    // &todo: Rework into optional measurements to support reporting of only the observed data
-    measurements: [count]Measurement = [_]Measurement{Measurement{}} ** count,
+    measurements: [max_measurement_count]?Measurement = [_]?Measurement{null} ** max_measurement_count,
     running: bool = true,
 
-    pub fn start(io: std.Io, id: Id) Self {
-        return .{ .io = io, .id = id, .start_ts = now(io) };
+    pub fn start(io: std.Io, desc: Descriptor) !Self {
+        var rv: Self = .{ .io = io, .ix = desc.ix, .start_ts = now(io) };
+        try rv.setup(desc);
+        return rv;
     }
 
     pub fn stop(self: *Self) !void {
-        try self.mark(self.id);
+        try self.mark(.{ .ix = self.ix });
         self.running = false;
     }
 
-    pub fn mark(self: *Self, id: Id) !void {
+    pub fn mark(self: *Self, desc: Descriptor) !void {
         if (!self.running)
             return error.NotRunning;
 
         const now_ts = now(self.io);
-        const elapse_ns = self.start_ts.durationTo(now_ts).nanoseconds;
-        const m = &self.measurements[@intFromEnum(self.id)];
-        m.sum += elapse_ns;
-        m.max = @max(m.max, elapse_ns);
 
-        self.id = id;
+        var m = self.measurements[self.ix].?;
+        {
+            const elapse_ns = self.start_ts.durationTo(now_ts).nanoseconds;
+            m.sum += elapse_ns;
+            m.max = @max(m.max, elapse_ns);
+        }
+        self.measurements[self.ix] = m;
+
+        try self.setup(desc);
+        self.ix = desc.ix;
         self.start_ts = now_ts;
     }
 
-    pub fn measurement(self: Self, id: Id) !Measurement {
+    pub fn measurement(self: Self, ix: usize) !?Measurement {
         if (self.running)
             return error.StillRunning;
-        return self.measurements[@intFromEnum(id)];
+        return self.measurements[ix];
+    }
+
+    pub fn format(self: Self, w: *std.Io.Writer) !void {
+        for (0..max_measurement_count) |ix| {
+            if (self.measurements[ix]) |m| {
+                try w.print("[Measurement]", .{});
+                const Local = struct {
+                    fn print(ts: Timestamp, desc: []const u8, ww: *std.Io.Writer) !void {
+                        const a = @divFloor(ts, 1_000_000_000);
+                        const b = ts - a * 1_000_000_000;
+                        try ww.print("({s}:{}.{:0>9.9}s)", .{ desc, a, @as(u64, @intCast(b)) });
+                    }
+                };
+                try Local.print(m.sum, "sum", w);
+                try Local.print(m.max, "max", w);
+                if (m.name) |name|
+                    try w.print("(name:{s})", .{name});
+                try w.print("(ix:{})\n", .{ix});
+                try w.flush();
+            }
+        }
+    }
+
+    fn setup(self: *Self, desc: Descriptor) !void {
+        if (desc.ix >= max_measurement_count)
+            return error.IxOutOfRange;
+
+        if (self.measurements[desc.ix]) |*m| {
+            if (desc.name) |name| {
+                if (m.name) |m_name| {
+                    if (!std.mem.eql(u8, name, m_name))
+                        return error.NameMismatch;
+                } else {
+                    m.name = name;
+                }
+            }
+        } else {
+            self.measurements[desc.ix] = Measurement{ .name = desc.name };
+        }
     }
 };
 
@@ -80,15 +119,14 @@ test "Scope" {
     const ut = std.testing;
 
     {
-        var s = Scope.start(ut.io, .A);
-        std.debug.print("A\n", .{});
-        try s.mark(.B);
-        std.debug.print("B\n", .{});
-        try s.mark(.A);
-        std.debug.print("A\n", .{});
+        var s = try Scope.start(ut.io, .{ .ix = 0 });
+        std.debug.print("0\n", .{});
+        try s.mark(.{ .ix = 1 });
+        std.debug.print("1\n", .{});
+        try s.mark(.{ .ix = 0, .name = "all" });
+        std.debug.print("0\n", .{});
         try s.stop();
 
-        std.debug.print("A {f}\n", .{try s.measurement(.A)});
-        std.debug.print("B {f}\n", .{try s.measurement(.B)});
+        std.debug.print("{f}\n", .{s});
     }
 }
