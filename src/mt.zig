3e79aa6629fff8a14cab6950723cc022cd0d56b3
Geert Fannes <geert.fannes@gmail.com>
2026-09-09 22:37:03 +0200

    wip

--- src/mt.zig
+++ src/mt.zig
@@ -9,19 +9,81 @@ pub const Runner = struct {
     a: std.mem.Allocator,
     io: std.Io,
 
+    state: State = undefined,
     workers: std.ArrayList(Worker) = .empty,
 
     pub fn init(self: *Self, worker_count: usize) !void {
+        self.state = .{ .io = self.io };
         try self.workers.resize(self.a, worker_count);
         for (self.workers.items, 0..) |*worker, ix0| {
-            try worker.init(self.a, ix0);
+            try worker.init(self.a, ix0, &self.state);
         }
     }
     pub fn deinit(self: *Self) void {
+        self.state.quit() catch {};
         for (self.workers.items) |*worker| {
             worker.deinit();
         }
         self.workers.deinit(self.a);
+    }
+
+    pub fn start(self: *Self) !void {
+        try self.state.start();
+    }
+};
+
+const State = struct {
+    const Self = @This();
+
+    const Value = enum { Idle, Running, Quit };
+
+    io: std.Io,
+
+    value: Value = .Idle,
+
+    mutex: std.Io.Mutex = .init,
+    cond: std.Io.Condition = .init,
+
+    fn run(self: *Self) !bool {
+        try self.mutex.lock(self.io);
+        defer self.mutex.unlock(self.io);
+        while (true)
+            switch (self.value) {
+                .Running => return true,
+                .Quit => return false,
+                else => try self.cond.wait(self.io, &self.mutex),
+            };
+    }
+
+    fn start(self: *Self) !void {
+        try self.mutex.lock(self.io);
+        defer self.mutex.unlock(self.io);
+        switch (self.value) {
+            .Quit => return,
+            .Running => return,
+            else => {
+                self.value = .Running;
+                self.cond.broadcast(self.io);
+            },
+        }
+    }
+    fn stop(self: *Self) !void {
+        try self.mutex.lock(self.io);
+        defer self.mutex.unlock(self.io);
+        switch (self.value) {
+            .Quit => return,
+            .Idle => return,
+            else => {
+                self.value = .Idle;
+                try self.cond.broadcast(self.io);
+            },
+        }
+    }
+    fn quit(self: *Self) !void {
+        try self.mutex.lock(self.io);
+        defer self.mutex.unlock(self.io);
+        self.value = .Quit;
+        self.cond.broadcast(self.io);
     }
 };
 
@@ -32,12 +94,14 @@ const Worker = struct {
 
     a: std.mem.Allocator,
     ix0: usize,
+    state: *State,
     thread: std.Thread,
     jobs: std.Deque(Job),
 
-    fn init(self: *Self, a: std.mem.Allocator, ix0: usize) !void {
+    fn init(self: *Self, a: std.mem.Allocator, ix0: usize, state: *State) !void {
         self.a = a;
         self.ix0 = ix0;
+        self.state = state;
         self.thread = try std.Thread.spawn(.{}, Worker.function, .{self});
         self.jobs = .empty;
     }
@@ -47,7 +111,9 @@ const Worker = struct {
     }
 
     fn function(self: *Self) !void {
-        std.debug.print("Worker {}\n", .{self.ix0});
+        while (try self.state.run()) {
+            std.debug.print("Worker {}\n", .{self.ix0});
+        }
     }
 };
 
@@ -57,4 +123,5 @@ test "mt" {
     var runner = Runner{ .a = ut.allocator, .io = ut.io };
     try runner.init(4);
     defer runner.deinit();
+    try runner.start();
 }
