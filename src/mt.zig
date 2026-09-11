606ead3756dd80c6bdddd17f3da6a64c4f77a1b8
Geert Fannes <geert.fannes@auro-3d.com>
2026-09-11 19:47:26 +0200

    wip on mt.Runner

--- src/mt.zig
+++ src/mt.zig
@@ -4,6 +4,7 @@ pub const Error = error{
     ExpectedIdleState,
     ExpectedRunningState,
     WaitingFailure,
+    WorkerIsClosed,
 };
 
 // A pool of workers, each with their own job queue.
@@ -15,18 +16,17 @@ pub const Runner = struct {
     a: std.mem.Allocator,
     io: std.Io,
 
-    cox: Cox = undefined,
+    busy_mask: BusyMask = undefined,
     workers: std.ArrayList(Worker) = .empty,
 
     pub fn init(self: *Self, worker_count: usize) !void {
-        self.cox = .{ .io = self.io };
+        self.busy_mask = try .init(self.io);
         try self.workers.resize(self.a, worker_count);
         for (self.workers.items, 0..) |*worker, ix0| {
-            try worker.init(self.a, self.io, ix0, &self.cox);
+            try worker.init(self.a, self.io, @intCast(ix0), &self.busy_mask);
         }
     }
     pub fn deinit(self: *Self) void {
-        self.cox.quit() catch {};
         for (self.workers.items) |*worker| {
             worker.deinit();
         }
@@ -34,151 +34,317 @@ pub const Runner = struct {
     }
 
     pub fn start(self: *Self, jobs_: []const Job) !void {
-        if (!try self.cox.is(.Idle))
-            return error.ExpectedIdleState;
-
         var jobs = jobs_;
 
-        self.cox.job_count = jobs.len;
-
+        std.debug.print("Pushing {} jobs ...", .{jobs_.len});
         while (jobs.len > 0) {
             for (self.workers.items) |*worker| {
                 if (jobs.len == 0)
                     break;
-                try worker.jobs.pushBack(self.a, jobs[0]);
+                try worker.push(jobs[0]);
                 jobs = jobs[1..];
             }
         }
+        std.debug.print(" done.\n", .{});
 
-        try self.cox.start();
+        for (self.workers.items) |*worker| {
+            try worker.changeState(.Running);
+        }
     }
 
     pub fn stop(self: *Self) !void {
-        if (!try self.cox.waitFor(.Done))
-            return error.WaitingFailure;
-    }
-};
+        // Wait for all workers to run out of work before we transition them to idle.
+        // Otherwise, new work can arrive and we have to process this with some workers already idle.
+        try self.busy_mask.waitForIdle();
 
-// Coxwain: controls the boat
-const Cox = struct {
-    const Self = @This();
-
-    const State = enum { Idle, Running, Done, Quit };
-
-    io: std.Io,
-
-    state: State = .Idle,
-    job_count: u64 = 0,
-
-    mutex: std.Io.Mutex = .init,
-    cond: std.Io.Condition = .init,
-
-    fn is(self: *Self, exp_state: State) !bool {
-        try self.mutex.lock(self.io);
-        defer self.mutex.unlock(self.io);
-        return self.state == exp_state;
-    }
-
-    fn waitFor(self: *Self, exp_state: State) !bool {
-        try self.mutex.lock(self.io);
-        defer self.mutex.unlock(self.io);
-        while (true) {
-            if (self.state == .Quit)
-                return false;
-            if (self.state == exp_state)
-                return true;
-            try self.cond.wait(self.io, &self.mutex);
+        for (self.workers.items) |*worker| {
+            try worker.changeState(.Idle);
         }
-    }
-
-    fn decrease(self: *Self, count: usize) !void {
-        try self.mutex.lock(self.io);
-        defer self.mutex.unlock(self.io);
-
-        self.job_count -= count;
-        if (self.job_count == 0)
-            self.state = .Done;
-    }
-
-    fn start(self: *Self) !void {
-        try self.mutex.lock(self.io);
-        defer self.mutex.unlock(self.io);
-        switch (self.state) {
-            .Quit => return,
-            .Running => return,
-            else => {
-                self.state = .Running;
-                self.cond.broadcast(self.io);
-            },
-        }
-    }
-    fn stop(self: *Self) !void {
-        try self.mutex.lock(self.io);
-        defer self.mutex.unlock(self.io);
-        switch (self.state) {
-            .Quit => return,
-            .Idle => return,
-            else => {
-                self.state = .Idle;
-                try self.cond.broadcast(self.io);
-            },
-        }
-    }
-    fn quit(self: *Self) !void {
-        try self.mutex.lock(self.io);
-        defer self.mutex.unlock(self.io);
-        self.state = .Quit;
-        self.cond.broadcast(self.io);
     }
 };
 
 const Job = struct {
+    io: std.Io,
     i: u64,
+    sum: *std.atomic.Value(u64),
+    // sum: *u64,
 
     pub fn deinit(_: *Job) void {}
 
+    pub fn run(self: *Job, worker: *Worker) !void {
+        if (false) {
+            std.debug.print("{f} sleeping\n", .{self.*});
+            try std.Io.sleep(self.io, .fromSeconds(@intCast(self.i)), .real);
+            std.debug.print("{f} awake\n", .{self.*});
+        } else {
+            if (self.i < 2 or self.i == 3000000) {
+                _ = self.sum.fetchAdd(self.i, .monotonic);
+                // self.sum.* += self.i;
+            } else {
+                const orig_i = self.i;
+
+                var job = self.*;
+
+                job.i = job.i / 2;
+                try worker.push(job);
+
+                job.i = orig_i - job.i;
+                try worker.push(job);
+            }
+        }
+    }
+
     pub fn format(self: Job, w: *std.Io.Writer) !void {
-        try w.print("[Job](i:{})\n", .{self.i});
+        try w.print("[Job](i:{})", .{self.i});
     }
 };
 
 const Worker = struct {
     const Self = @This();
 
+    const State = enum { Init, Idle, Running, Deinit };
+
     a: std.mem.Allocator,
     io: std.Io,
-    ix0: usize,
-    cox: *Cox,
+
+    ix0: u6,
+    busy_mask: *BusyMask,
     thread: std.Thread,
 
-    queue: Queue,
+    busy: bool = false,
 
-    fn init(self: *Self, a: std.mem.Allocator, io: std.Io, ix0: usize, cox: *Cox) !void {
-        self.a = a;
-        self.io = io;
-        self.ix0 = ix0;
-        self.cox = cox;
-        self.thread = try std.Thread.spawn(.{}, Worker.function, .{self});
-        self.queue = .{ .a = a, .io = io };
+    wnt_state: State = .Init,
+    act_state: State = .Init,
+    queue: std.Deque(Job) = .empty,
+    mutex: std.Io.Mutex = .init,
+    cond: std.Io.Condition = .init,
+
+    fn init(self: *Self, a: std.mem.Allocator, io: std.Io, ix0: u6, busy_mask: *BusyMask) !void {
+        self.* = .{
+            .a = a,
+            .io = io,
+            .ix0 = ix0,
+            .busy_mask = busy_mask,
+            .thread = try std.Thread.spawn(.{}, Worker.function, .{self}),
+        };
+        try self.changeState(.Idle);
     }
     fn deinit(self: *Self) void {
+        self.changeState(.Deinit) catch {};
+
         self.thread.join();
-        self.queue.deinit();
+
+        self.mutex.lock(self.io) catch {};
+        defer self.mutex.unlock(self.io);
+        while (self.queue.popBack()) |const_job| {
+            var job = const_job;
+            job.deinit();
+        }
+        self.queue.deinit(self.a);
+    }
+
+    fn changeState(self: *Self, state: State) !void {
+        // Update wnt_state
+        {
+            try self.mutex.lock(self.io);
+            defer self.mutex.unlock(self.io);
+
+            if (state != self.wnt_state) {
+                self.wnt_state = state;
+                self.cond.signal(self.io);
+            }
+        }
+
+        // Wait until act_state follows
+        {
+            try self.mutex.lock(self.io);
+            defer self.mutex.unlock(self.io);
+
+            while (self.act_state != state) {
+                try self.cond.wait(self.io, &self.mutex);
+            }
+        }
+    }
+
+    fn push(self: *Self, job: Job) !void {
+        try self.mutex.lock(self.io);
+        defer self.mutex.unlock(self.io);
+        if (self.act_state == .Deinit)
+            return error.WorkerIsClosed;
+        try self.queue.pushBack(self.a, job);
     }
 
     fn function(self: *Self) !void {
-        while (try self.cox.waitFor(.Running)) {
-            std.debug.print("Worker {} has {} jobs\n", .{ self.ix0, try self.queue.len() });
+        var quit = false;
+        while (!quit) {
+            var maybe_job: ?Job = null;
+            {
+                try self.mutex.lock(self.io);
+                defer self.mutex.unlock(self.io);
 
-            var maybe_job = try self.queue.pop();
+                {
+                    const new_busy = self.queue.len > 0;
+                    if (new_busy != self.busy) {
+                        self.busy = new_busy;
+                        try self.busy_mask.set(self.ix0, self.busy);
+                    }
+                }
+                if (self.act_state != self.wnt_state) {
+                    std.debug.print("[Worker](ix0:{})(act_state:{})(wnt_state:{})(len:{})\n", .{ self.ix0, self.act_state, self.wnt_state, self.queue.len });
+
+                    const change_state = switch (self.wnt_state) {
+                        .Idle => !self.busy,
+                        else => true,
+                    };
+
+                    if (change_state) {
+                        self.act_state = self.wnt_state;
+                        self.cond.signal(self.io);
+                    }
+                }
+
+                switch (self.act_state) {
+                    .Running => {
+                        maybe_job = self.queue.popFront();
+                        if (maybe_job == null)
+                            try self.cond.wait(self.io, &self.mutex);
+                    },
+                    .Deinit => quit = true,
+                    else => try self.cond.wait(self.io, &self.mutex),
+                }
+            }
+
             if (maybe_job) |*job| {
                 defer job.deinit();
-                std.log.info("{f}", .{job});
-                try self.cox.decrease(1);
+                try job.run(self);
             }
         }
     }
 };
+
+const BusyMask = struct {
+    const Self = @This();
+
+    io: std.Io,
+
+    mask: u64 = 0,
+    mutex: std.Io.Mutex = .init,
+    cond: std.Io.Condition = .init,
+
+    fn init(io: std.Io) !Self {
+        var rv = Self{
+            .io = io,
+        };
+
+        {
+            // Block is necessary to avoid moving a locked mutex
+            try rv.mutex.lock(rv.io);
+            defer rv.mutex.unlock(rv.io);
+            rv.mask = 0;
+        }
+
+        return rv;
+    }
+
+    fn set(self: *Self, ix0: u6, b: bool) !void {
+        try self.mutex.lock(self.io);
+        defer self.mutex.unlock(self.io);
+
+        const old_mask = self.mask;
+
+        const m: u64 = @as(u64, 1) << ix0;
+
+        if (b)
+            self.mask |= m
+        else
+            self.mask &= ~m;
+
+        if (self.mask != old_mask) {
+            std.debug.print("[Busy](mask:0x{x})\n", .{self.mask});
+            self.cond.broadcast(self.io);
+        }
+    }
+
+    fn waitForIdle(self: *Self) !void {
+        try self.mutex.lock(self.io);
+        defer self.mutex.unlock(self.io);
+        while (self.mask != 0) {
+            try self.cond.wait(self.io, &self.mutex);
+        }
+    }
+
+    pub fn format(self: Self, w: *std.Io.Writer) !void {
+        try w.print("[BusyMask](mask:0x{x})", .{self.mask});
+    }
+};
+
+test "mt.Runner" {
+    const ut = std.testing;
+
+    std.debug.print("mt.Runner\n", .{});
+
+    var runner = Runner{ .a = ut.allocator, .io = ut.io };
+    try runner.init(50);
+    defer runner.deinit();
+
+    var sum = std.atomic.Value(u64).init(0);
+    // var sum: u64 = 0;
+
+    const jobs = [_]Job{
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+        .{ .io = ut.io, .i = 1000000, .sum = &sum },
+        .{ .io = ut.io, .i = 2000000, .sum = &sum },
+        .{ .io = ut.io, .i = 3000000, .sum = &sum },
+        .{ .io = ut.io, .i = 4000000, .sum = &sum },
+    };
+    try runner.start(&jobs);
+    try runner.stop();
+
+    std.debug.print("Sum {}\n", .{sum});
+}
 
 pub const Queue = struct {
     pub const Self = @This();
@@ -229,18 +395,6 @@ pub const Queue = struct {
         return self.deque.len;
     }
 };
-
-test "mt.Runner" {
-    const ut = std.testing;
-
-    var runner = Runner{ .a = ut.allocator, .io = ut.io };
-    try runner.init(4);
-    defer runner.deinit();
-
-    // const jobs = [_]Job{Job{ .i = 12 }};
-    // try runner.start(&jobs);
-    // try runner.stop();
-}
 
 test "mt.Queue" {
     const ut = std.testing;
